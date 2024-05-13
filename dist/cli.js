5db9b358dce07aa0f15d8eddd52191e3f02a9e6f
Benny Kerido <kerido112@gmail.com>
2024-05-13 13:53:46 +0300

    Add CLI

---
+++ dist/cli.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+"use strict";
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    console.log('Usage: smart-styles update');
+    process.exit(1);
+}
+const command = args[0];
+switch (command) {
+    case 'update':
+        console.log('Updating config file');
+        require('./lib/initialization');
+        console.log('Config file successfully updated');
+        break;
+    default:
+        console.log('Unknown Command');
+        process.exit(1);
+}
