c8f7fc4c6c0524c015c531b6571f8304c66865a4
Benny Kerido <kerido112@gmail.com>
2024-05-13 13:37:37 +0300

    Fix import

--- dist/lib/initialization.js
+++ dist/lib/initialization.js
@@ -2,48 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 const configFileName = 'smart-styles.config.json';
-function find(...args) {
-    var _a;
-    const rel = path.join.apply(null, [].slice.call(args));
-    return findStartingWith(path.dirname((_a = process.mainModule) === null || _a === void 0 ? void 0 : _a.filename), rel);
-}
-function findStartingWith(start, rel) {
-    const file = path.join(start, rel);
-    try {
-        fs.statSync(file);
-        return file;
-    }
-    catch (err) {
-        if (path.dirname(start) !== start) {
-            return findStartingWith(path.dirname(start), rel);
-        }
-    }
-}
-function parse(content) {
-    if (/^\s*{/.test(content)) {
-        return JSON.parse(content);
-    }
-    return {};
-}
-function file(...args) {
-    const nonNullArgs = [].slice.call(args).filter(arg => arg !== null);
-    for (let i = 0; i < nonNullArgs.length; i++) {
-        if (typeof nonNullArgs[i] !== 'string') {
-            return;
-        }
-    }
-    const file = path.join.apply(null, nonNullArgs);
-    try {
-        return fs.readFileSync(file, 'utf-8');
-    }
-    catch (err) {
-        return undefined;
-    }
-}
-function json(...args) {
-    const content = file.apply(null, args);
-    return content ? parse(content) : {};
-}
 function findRootDir(startDir) {
     let currentDir = startDir;
     while (currentDir !== path.parse(currentDir).root) {
@@ -52,7 +10,7 @@ function findRootDir(startDir) {
         }
         currentDir = path.dirname(currentDir); // Move one directory up
     }
-    throw new Error(`Could not find the root directory with ${file}`);
+    return undefined;
 }
 function readConfigFile(rootDir, configFile = configFileName) {
     const configPath = path.join(rootDir, configFile);
@@ -68,5 +26,7 @@ function writeToConfigFile(content) {
     fs.writeFileSync(outputPath, `export default ${content}`);
 }
 const rootDir = findRootDir(process.cwd());
-const config = readConfigFile(rootDir);
-writeToConfigFile(config);
+if (rootDir) {
+    const config = readConfigFile(rootDir);
+    writeToConfigFile(config);
+}
--- lib/initialization.ts
+++ lib/initialization.ts
@@ -1,51 +1,6 @@
 const fs = require('fs');
 const path = require('path')
 const configFileName = 'smart-styles.config.json';
-function find(...args: string[]) {
-    const rel = path.join.apply(null, [].slice.call(args));
-    return findStartingWith(path.dirname(process.mainModule?.filename), rel);
-}
-
-function findStartingWith(start: string, rel: string) {
-    const file = path.join(start, rel);
-    try {
-        fs.statSync(file);
-        return file;
-    } catch (err: any) {
-        if (path.dirname(start) !== start) {
-            return findStartingWith(path.dirname(start), rel);
-        }
-    }
-}
-
-function parse(content: any) {
-    if (/^\s*{/.test(content)) {
-        return JSON.parse(content);
-    }
-    return {};
-}
-
-function file(...args: any[]) {
-    const nonNullArgs = [].slice.call(args).filter(arg => arg !== null);
-
-    for (let i = 0; i < nonNullArgs.length; i++) {
-        if (typeof nonNullArgs[i] !== 'string') {
-            return;
-        }
-    }
-
-    const file = path.join.apply(null, nonNullArgs);
-    try {
-        return fs.readFileSync(file, 'utf-8');
-    } catch (err: any) {
-        return undefined;
-    }
-}
-
-function json(...args: string[]) {
-    const content = file.apply(null, args);
-    return content ? parse(content) : {};
-}
 
 function findRootDir(startDir: string) {
     let currentDir = startDir;
@@ -56,7 +11,7 @@ function findRootDir(startDir: string) {
         currentDir = path.dirname(currentDir); // Move one directory up
     }
 
-    throw new Error(`Could not find the root directory with ${file}`);
+    return undefined;
 }
 
 function readConfigFile(rootDir: string, configFile = configFileName) {
@@ -74,5 +29,7 @@ function writeToConfigFile(content: string) {
 }
 
 const rootDir = findRootDir(process.cwd());
-const config = readConfigFile(rootDir);
-writeToConfigFile(config);
+if (rootDir) {
+    const config = readConfigFile(rootDir);
+    writeToConfigFile(config);
+}
