670cd929146219c0584c395cfefc20926619ca85
Benny Kerido <kerido112@gmail.com>
2024-05-13 12:33:32 +0300

    Push build

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.tc = exports.themeColor = exports.setColorsPalette = exports.setFontFamilies = exports.useTheme = exports.SmartStyles = void 0;
+require("./lib/initialization");
+const utils_1 = require("./lib/utils");
+Object.defineProperty(exports, "setFontFamilies", { enumerable: true, get: function () { return utils_1.setFontFamilies; } });
+Object.defineProperty(exports, "setColorsPalette", { enumerable: true, get: function () { return utils_1.setColorsPalette; } });
+Object.defineProperty(exports, "themeColor", { enumerable: true, get: function () { return utils_1.themeColor; } });
+Object.defineProperty(exports, "tc", { enumerable: true, get: function () { return utils_1.tc; } });
+const SmartStyles_1 = __importDefault(require("./lib/SmartStyles"));
+exports.SmartStyles = SmartStyles_1.default;
+const useTheme_1 = __importDefault(require("./lib/useTheme"));
+exports.useTheme = useTheme_1.default;
---
+++ dist/lib/initialization.js
@@ -0,0 +1,52 @@
+"use strict";
+var _a, _b;
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("./utils");
+const fs = require('fs');
+const path = require('path');
+function find(...args) {
+    var _a;
+    const rel = path.join.apply(null, [].slice.call(args));
+    return findStartingWith(path.dirname((_a = process.mainModule) === null || _a === void 0 ? void 0 : _a.filename), rel);
+}
+function findStartingWith(start, rel) {
+    const file = path.join(start, rel);
+    try {
+        fs.statSync(file);
+        return file;
+    }
+    catch (err) {
+        if (path.dirname(start) !== start) {
+            return findStartingWith(path.dirname(start), rel);
+        }
+    }
+}
+function parse(content) {
+    if (/^\s*{/.test(content)) {
+        return JSON.parse(content);
+    }
+    return {};
+}
+function file(...args) {
+    const nonNullArgs = [].slice.call(args).filter(arg => arg !== null);
+    for (let i = 0; i < nonNullArgs.length; i++) {
+        if (typeof nonNullArgs[i] !== 'string') {
+            return;
+        }
+    }
+    const file = path.join.apply(null, nonNullArgs);
+    try {
+        return fs.readFileSync(file, 'utf-8');
+    }
+    catch (err) {
+        return undefined;
+    }
+}
+function json(...args) {
+    const content = file.apply(null, args);
+    return content ? parse(content) : {};
+}
+const configPath = find('smart-styles.config.json');
+const configObject = json(configPath);
+(0, utils_1.setFontFamilies)((_a = configObject.fonts) !== null && _a !== void 0 ? _a : {});
+(0, utils_1.setColorsPalette)((_b = configObject.colors) !== null && _b !== void 0 ? _b : {});
