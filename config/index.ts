f10ceb10fd2f78470c8d3474abe0599dc40c43dc
Benny Kerido <kerido112@gmail.com>
2024-05-26 16:11:16 +0300

    CLI issue

--- config/index.ts
+++ config/index.ts
@@ -1,1 +1,7 @@
-export default {};
+type ConfigFile = {
+    baseWidth?: number;
+    baseHeight?: number;
+    colors?: Record<string, string>;
+    fonts?: Record<string, string>;
+};
+export default {} as ConfigFile;
